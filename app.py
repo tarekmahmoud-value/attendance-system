e9911ac2a99f0fc1ce9af1dc01bd4178da6c3448
tarekmahmoud-value <tarekmahmoud.value@gmail.com>
2025-08-02 09:20:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Attendance System Running!'
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/attendance')
+def attendance():
+    return render_template('attendance_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
