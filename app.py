3b95693c6c75c9585e05ca573b189dc81cd90107
tarekmahmoud-value <tarekmahmoud.value@gmail.com>
2025-08-02 08:04:29 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Attendance System Running!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
